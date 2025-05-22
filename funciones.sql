61a4a05f9c4f8bdb17075219758c39422acad6de
julianandres31 <julianandrestriana81@gmail.com>
2025-05-22 17:29:10 -0500

    Add files via upload

--- funciones.sql
+++ funciones.sql
@@ -1,3 +1,5 @@
+/*1*/
+
 USE `peliculas`;
 DROP function IF EXISTS `fn_dias_retraso`;
 
@@ -10,6 +12,97 @@ BEGIN
     DECLARE dias INT;
     SET dias = DATEDIFF(fecha_entrega, fecha_tope);
     RETURN IF(dias > 0, dias, 0);
+END$$
+
+DELIMITER ;
+
+/*2*/
+
+USE `peliculas`;
+DROP function IF EXISTS `contar_copias_por_titulo`;
+
+DELIMITER $$
+USE `peliculas`$$
+CREATE FUNCTION contar_copias_por_titulo(titulo_p VARCHAR(50)) 
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE total INT;
+    SELECT COUNT(*) INTO total
+    FROM copias as c
+    JOIN pelicula as p ON c.id_pelicula = p.id_pelicula
+    WHERE p.titulo = titulo_p;
+    RETURN total;
+END$$
+
+DELIMITER ;
+
+/*3*/
+
+USE `peliculas`;
+DROP function IF EXISTS `dias_retraso`;
+
+DELIMITER $$
+USE `peliculas`$$
+CREATE FUNCTION dias_retraso(id INT) 
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE retraso INT;
+    SELECT DATEDIFF(fecha_entrega, fecha_tope) INTO retraso
+    FROM prestamos
+    WHERE id_prestamo = id;
+
+    IF retraso < 0 THEN
+        SET retraso = 0;
+    END IF;
+
+    RETURN retraso;
+END$$
+
+DELIMITER ;
+
+/*4*/
+
+USE `peliculas`;
+DROP function IF EXISTS `nombre_completo_cliente`;
+
+DELIMITER $$
+USE `peliculas`$$
+CREATE FUNCTION nombre_completo_cliente(id INT) 
+RETURNS VARCHAR(50)
+DETERMINISTIC
+BEGIN
+    DECLARE nombre_completo VARCHAR(50);
+    SELECT CONCAT(nombre, ' ', apellido) INTO nombre_completo
+    FROM clientes
+    WHERE id_cliente = id;
+    RETURN nombre_completo;
+END$$
+
+DELIMITER ;
+
+/*5*/
+
+USE `peliculas`;
+DROP function IF EXISTS `total_recaudo_por_pelicula`;
+
+DELIMITER $$
+USE `peliculas`$$
+CREATE FUNCTION total_recaudo_por_pelicula(idPeli INT) 
+RETURNS DOUBLE
+DETERMINISTIC
+BEGIN
+    DECLARE total DOUBLE;
+    SELECT SUM(precio_alquiler) INTO total
+    FROM copias
+    WHERE id_pelicula = idPeli;
+    
+    IF total IS NULL THEN
+        SET total = 0;
+    END IF;
+    
+    RETURN total;
 END$$
 
 DELIMITER ;
--- vistas.sql
+++ vistas.sql
@@ -1,3 +1,5 @@
+/*1*/
+
 USE `peliculas`;
 CREATE  OR REPLACE VIEW vista_prestamos_activos AS
 SELECT 
@@ -13,4 +15,77 @@ FROM prestamos pr
 JOIN clientes cl ON pr.id_cliente = cl.id_cliente
 JOIN copias c ON pr.id_copia = c.id_copia
 JOIN pelicula p ON c.id_pelicula = p.id_pelicula
-WHERE pr.fecha_entrega IS NULL;
+WHERE pr.fecha_entrega IS NULL;;
+
+/*2*/
+
+USE `peliculas`;
+CREATE  OR REPLACE VIEW vista_peliculas_con_precios AS
+SELECT 
+    p.id_pelicula,
+    p.titulo,
+    c.id_copia,
+    c.precio_alquiler
+FROM 
+    pelicula p
+JOIN 
+    copias c ON p.id_pelicula = c.id_pelicula;;
+
+/*3*/    
+    
+    USE `peliculas`;
+CREATE  OR REPLACE VIEW vista_historial_prestamos AS
+SELECT 
+    pr.id_prestamo,
+    cl.nombre,
+    cl.apellido,
+    p.titulo,
+    pr.fecha_prestamo,
+    pr.fecha_tope,
+    pr.fecha_entrega
+FROM 
+    prestamos pr
+JOIN 
+    clientes cl ON pr.id_cliente = cl.id_cliente
+JOIN 
+    copias c ON pr.id_copia = c.id_copia
+JOIN 
+    pelicula p ON c.id_pelicula = p.id_pelicula;;
+
+/*4*/
+
+    USE `peliculas`;
+CREATE  OR REPLACE VIEW vista_clientes_con_prestamos_vencidos AS
+SELECT 
+    cl.id_cliente,
+    cl.nombre,
+    cl.apellido,
+    p.titulo,
+    pr.fecha_prestamo,
+    pr.fecha_tope
+FROM 
+    prestamos pr
+JOIN 
+    clientes cl ON pr.id_cliente = cl.id_cliente
+JOIN 
+    copias c ON pr.id_copia = c.id_copia
+JOIN 
+    pelicula p ON c.id_pelicula = p.id_pelicula
+WHERE 
+    pr.fecha_entrega IS NULL
+    AND pr.fecha_tope < CURDATE();;
+    
+/*5*/
+
+USE `peliculas`;
+CREATE  OR REPLACE VIEW vista_total_copias_por_pelicula AS
+SELECT 
+    p.id_pelicula,
+    p.titulo,
+    COUNT(c.id_copia) AS total_copias
+FROM 
+    pelicula p
+LEFT JOIN 
+    copias c ON p.id_pelicula = c.id_pelicula
+GROUP BY 
+    p.id_pelicula, p.titulo;;
